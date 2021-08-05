afc8406a7b630984df0d556612c338cb76b9dd27
Matt Jacobson <mhjacobson@me.com>
2021-08-04 22:53:24 -0400

    misc.s: remove workaround for now-merged LLVM bug

--- misc.s
+++ misc.s
@@ -9,8 +9,3 @@
 
 .globl _objc_empty_vtable
 .equ _objc_empty_vtable, 0
-
-; Force libgcc's __do_global_ctors to be linked in, so that our initializer runs.
-; TODO: remove this once this LLVM fix goes in:
-; <https://reviews.llvm.org/D107133>
-.globl __do_global_ctors
