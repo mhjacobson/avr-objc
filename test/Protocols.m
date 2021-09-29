967fff902f1ec2e97bc4d0a77efb5a4c5f55c668
Matt Jacobson <mhjacobson@me.com>
2021-09-29 18:26:27 -0400

    Protocols.m: add more protocols tests

--- test/Protocols.m
+++ test/Protocols.m
@@ -40,6 +40,20 @@
         TEST_ASSERT(class_conformsToProtocol([ProtocolsFoo self], @protocol(Proto)));
         // TEST_ASSERT(class_conformsToProtocol([ProtocolsBar self], @protocol(Proto)));
         TEST_ASSERT(protocol_conformsToProtocol(@protocol(Proto2), @protocol(Proto)));
+        TEST_ASSERT(!protocol_conformsToProtocol(@protocol(Proto), @protocol(Proto2)));
+
+        const Protocol *const proto = objc_getProtocol("Proto");
+        TEST_ASSERT(proto != nil);
+        TEST_ASSERT(proto == @protocol(Proto));
+
+        const Protocol *const proto2 = objc_getProtocol("Proto2");
+        TEST_ASSERT(proto2 != nil);
+        TEST_ASSERT(proto2 == @protocol(Proto2));
+
+        TEST_ASSERT([ProtocolsFoo conformsToProtocol:@protocol(Proto)]);
+        // TEST_ASSERT([ProtocolsBar conformsToProtocol:@protocol(Proto)]);
+        TEST_ASSERT([proto2 conformantClassesConformToProtocol:@protocol(Proto)]);
+        TEST_ASSERT(![proto conformantClassesConformToProtocol:@protocol(Proto2)]);
 
         printf("Tests passed.\n");
         for (;;) ;
